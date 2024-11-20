f591e59dab2dfb7ee13a45f1cb0f097adaf7bf36
Masaki Suzuki <masakisuzuki.shkr@gmail.com>
2024-11-21 06:41:06 +0900

    メンバーページを作成

--- app/_components/Header/index.tsx
+++ app/_components/Header/index.tsx
@@ -1,10 +1,11 @@
 import Image from "next/image";
+import Link from "next/link";
 import styles from "./index.module.css";
 
 export default function Header() {
   return (
     <header className={styles.header}>
-      <a href="/" className={styles.logoLink}>
+      <Link href="/" className={styles.logoLink}>
         <Image
           src="/logo.svg"
           alt="SIMPLE"
@@ -13,7 +14,14 @@ export default function Header() {
           height={133}
           priority
         />
-      </a>
+      </Link>
+      <nav>
+        <ul className={styles.items}>
+          <li>
+            <Link href="/members">メンバー</Link>
+          </li>
+        </ul>
+      </nav>
     </header>
   );
 }
---
+++ app/members/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return(
+    <div style={{ magin: 20 }}>
+      <h1>メンバーページ</h1>
+    </div>
+  );
+}
