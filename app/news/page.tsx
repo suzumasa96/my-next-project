9664797add3f546480c9d15e66a9f88f9d10495a
Masaki Suzuki <masakisuzuki.shkr@gmail.com>
2024-11-28 07:33:38 +0900

    検索機能を実装

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -1,6 +1,7 @@
 import { getNewsList } from '@/app/_libs/microcms';
 import NewsList from '@/app/_components/NewsList';
 import Pagination from '../_components/Pagination';
+import SearchField from '../_components/SearchField';
 import { NEWS_LIST_LIMIT } from '@/app/_constants';
 
 export default async function Page() {
@@ -10,6 +11,7 @@ export default async function Page() {
 
   return (
     <>
+      <SearchField />
       <NewsList news={news} />
       <Pagination totalCount={totalCount} />
     </>
